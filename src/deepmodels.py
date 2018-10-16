b3ea03fb5606369112ca7d6d3d11b76716a70576
RParedesPalacios <rparedes@risen.dsic.upv.es>
2018-10-16 10:12:19 +0200

    examples

--- src/deepmodels.py
+++ src/deepmodels.py
@@ -29,13 +29,13 @@ def eval_class_model(Xt,Lt,args):
         model.summary()
 
     model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
-    
+
     if (args.tsdir==None):
         ts_steps=len(Xt)//batch_size
     else:
         ts_steps=args.numts//batch_size
 
-    print("Evaluating...")   
+    print("Evaluating...")
     score=model.evaluate_generator(test_generator(args,Xt,Lt),ts_steps)
     print("Acc:%.2f%%" % (score[1]*100))
 
@@ -50,17 +50,17 @@ def train_class_model(X,L,TEST,Xt,Lt,args):
     if (args.trdir==None):
         num_classes=len(set(L))
     else:
-        gen=ImageDataGenerator().flow_from_directory(args.trdir,target_size=(args.height,args.width),  
+        gen=ImageDataGenerator().flow_from_directory(args.trdir,target_size=(args.height,args.width),
             batch_size=args.batch,
             class_mode='categorical')
         num_classes=len(set(gen.classes))
         numtr=gen.samples
 
     if (args.tsdir!=None):
-        gen=ImageDataGenerator().flow_from_directory(args.tsdir,target_size=(args.height,args.width),  
+        gen=ImageDataGenerator().flow_from_directory(args.tsdir,target_size=(args.height,args.width),
             batch_size=args.batch,
             class_mode='categorical')
-        numts=gen.samples    
+        numts=gen.samples
 
 
     ######### MODELS
@@ -85,7 +85,7 @@ def train_class_model(X,L,TEST,Xt,Lt,args):
         L = keras.utils.to_categorical(L, num_classes)
         Lt = keras.utils.to_categorical(Lt, num_classes)
 
-    
+
     #### GENERAL
     batch_size=args.batch
     print("Batch size",batch_size)
@@ -94,7 +94,7 @@ def train_class_model(X,L,TEST,Xt,Lt,args):
     if (PRETR):
         fepochs=args.fepochs
         print("Fepochs",fepochs)
-        
+
     #### OPTIMIZER
     if (args.optim=="sgd"):
         print ("Using SGD")
@@ -105,11 +105,11 @@ def train_class_model(X,L,TEST,Xt,Lt,args):
     elif (args.optim=="rmsprop"):
         print ("Using RMSprop")
         opt=RMSprop(lr=args.lr, rho=0.9, epsilon=None, decay=0.0)
-        
-    
+
+
     if (args.trdir==None):
         tr_steps=len(X) // batch_size
-        if (TEST): 
+        if (TEST):
             ts_steps=len(Xt)//batch_size
     else:
         tr_steps=numtr//batch_size
@@ -125,14 +125,14 @@ def train_class_model(X,L,TEST,Xt,Lt,args):
                 if (layer.name==args.flayer):
                     break
                 else:
-                    layer.trainable = False 
-        else:    
+                    layer.trainable = False
+        else:
             print("Freezing the pre-trained model %d epochs" %(fepochs))
             for layer in base.layers:
                 layer.trainable = False
-            
+
         model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
-        
+
         if (TEST):
             model.fit_generator(training_generator(args,num_classes,X,L),
                             steps_per_epoch=tr_steps,
@@ -145,21 +145,21 @@ def train_class_model(X,L,TEST,Xt,Lt,args):
                             steps_per_epoch=tr_steps,
                             epochs=fepochs,
                             verbose=1)
-        
+
         for layer in base.layers:
             layer.trainable = True
 
         print("Setting all trainable")
-        
 
-        
+
+
     ## REGULAR TRAINING
     if (args.lra=="yes"):
         e1=int(epochs*0.25)
         e2=int(epochs*0.5)
         print ("Learning rate annealing epochs: 0-->",e1,"-->",e2,"-->",epochs)
         print ("Learning rate annealing LR:",args.lr,"-->",args.lr/10,"-->",args.lr/100)
-        
+
         def scheduler(epoch):
             if epoch < e1:
                 return args.lr
@@ -176,7 +176,7 @@ def train_class_model(X,L,TEST,Xt,Lt,args):
 
     model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
 
-    
+
     if (TEST):
         model.fit_generator(training_generator(args,num_classes,X,L),
                             steps_per_epoch=tr_steps,
@@ -195,30 +195,3 @@ def train_class_model(X,L,TEST,Xt,Lt,args):
     ## SAVE MODEL
     if (args.save_model!=None):
         save_json_model(model,args.save_model)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
