f14f3a3f26ac8993dfa945ab780f95fe5917cef6
RParedesPalacios <rparedes@risen.dsic.upv.es>
2018-10-22 12:20:54 +0200

    src

--- src/automodel.py
+++ src/automodel.py
@@ -1,49 +1,48 @@
 import math
 import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization as BN
-from keras.layers import GaussianNoise as GN
+from keras import models
+from keras import layers
+import numpy as np
 
 
-def basic_block(model,K,args,ishape=0):
-    if (ishape!=0):
-        model.add(Conv2D(K, (3, 3), padding='same',input_shape=ishape))
-    else:
-        model.add(Conv2D(K, (3, 3), padding='same'))
 
-    model.add(BN())
-    if (args.da_gauss!=0.0):
-        model.add(GN(args.da_gauss))
-    model.add(Activation('relu'))
+def basic_block(y,K,args,ishape=0,residual=0):
 
-    for i in range(args.autonconv-1):
-        model.add(Conv2D(K, (3, 3), padding='same'))
-        model.add(BN())
+    if (residual):
+        x=y
+
+    str=np.ones(args.autonconv)
+    if (residual):
+        str[args.autonconv-1]=2
+    str=np.int32(str)
+
+    for i in range(args.autonconv):
+        y=layers.Conv2D(K, kernel_size=(3, 3), strides=(str[i],str[i]), padding='same')(y)
+        y=layers.BatchNormalization()(y)
         if (args.da_gauss!=0.0):
-            model.add(GN(args.da_gauss))
-        model.add(Activation('relu'))
+            y=layers.GaussianNoise(0.3)(y)
+        if (residual==0)|(i<args.autonconv-1):
+            y=layers.ReLU()(y)
 
 
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    return model
+    if (residual):
+         x=layers.Conv2D(K, kernel_size=(1, 1), strides=(2,2),padding='same')(x)
+         y=layers.add([x, y])
+         y=layers.ReLU()(y)
 
-
-def auto_model(X,L,args,num_classes):
-
-    if (args.trdir==None):
-        h=X.shape[1]
-        w=X.shape[2]
-        num_classes=len(set(L))
-        shape=X.shape[1:]
     else:
-        h=args.height
-        w=args.width
-        if (args.chan=="rgb"):
-            shape=(h,w,3)
-        else:
-            shape=(h,w,1)
+        y=layers.MaxPooling2D(pool_size=(2, 2))(y)
+    return y
+
+
+def auto_model(args,num_classes):
+
+    h=args.height
+    w=args.width
+    if (args.chan=="rgb"):
+        shape=(h,w,3)
+    else:
+        shape=(h,w,1)
 
     s=min(h,w)
 
@@ -53,26 +52,33 @@ def auto_model(X,L,args,num_classes):
 
     print("Depth=",DEPTH)
 
-    model = Sequential()
+    ##model = Sequential()
     numf=int(KINI/2)
     for i in range(DEPTH):
         numf=numf*2
         if (numf>KEND):
             numf=KEND
 
+        res=0
+        if ((args.auto_res==True)&(DEPTH>1)):
+            res=1
+
         if (i==0):
-            model=basic_block(model,numf,args,shape)
+            image_tensor = layers.Input(shape=shape)
+            x=basic_block(image_tensor,numf,args)
         else:
-            model=basic_block(model,numf,args)
+            x=basic_block(x,numf,args,residual=res)
 
 
-    model.add(Flatten())
+    x=layers.Flatten()(x)
     for i in range(args.autodlayers):
-        model.add(Dense(args.autodsize))
-        model.add(BN())
-        model.add(Activation('relu'))
+        x=layers.Dense(args.autodsize)(x)
+        x=layers.BatchNormalization()(x)
+        if (args.da_gauss!=0.0):
+            x=layers.GaussianNoise(0.3)(x)
+        x=layers.ReLU()(x)
 
-    model.add(Dense(num_classes))
-    model.add(Activation('softmax'))
+    x=layers.Dense(num_classes, activation='softmax')(x)
+    model = models.Model(inputs=[image_tensor], outputs=[x])
 
     return model
