561f3e3d949d7dd2a584a0015f5d4d54c20e368a
RParedesPalacios <rparedes@dsic.upv.es>
2018-11-14 12:13:23 +0100

    src

--- src/detect_loss.py
+++ src/detect_loss.py
@@ -7,12 +7,12 @@ def hnm_loss(y_true,y_pred):
     yt=tf.reshape(y_true,[-1])
     yp=tf.reshape(y_pred,[-1])
 
-    ## Count positives and define neg as 3*pos
+    ## Count positives and define neg as pos
     pos=tf.cast(tf.count_nonzero(yt),dtype=tf.int32)
     if (pos==0):
         return 0.0
     else:
-        neg=tf.maximum(3*pos,1)
+        neg=pos
         neg=tf.cast(neg,dtype=tf.int32)
 
         zero=tf.constant(0, dtype=tf.float32)
@@ -26,8 +26,8 @@ def hnm_loss(y_true,y_pred):
         indneg=tf.cast(tf.reshape(indneg,[-1]),dtype=tf.int32)
         yp_n=tf.gather(yp,indneg)
         yp_n,ind=tf.nn.top_k(yp_n,neg)
-        mask=tf.greater(yp_n, 0.5)
-        yp_n=tf.boolean_mask(yp_n, mask)
+        #mask=tf.greater(yp_n, 0.5)
+        #yp_n=tf.boolean_mask(yp_n, mask)
 
         ##Concat predicted both
         yp=tf.concat([yp_p,yp_n],0)
@@ -48,28 +48,18 @@ def score_pos(y_true, y_pred):
     yt=tf.reshape(y_true,[-1])
     yp=tf.reshape(y_pred,[-1])
 
-
-    ## Count positives and define neg
+    ## Count positives and define neg as pos
     pos=tf.cast(tf.count_nonzero(yt),dtype=tf.int32)
     if (pos==0):
         return 0.0
     else:
-        neg=pos
-        neg=tf.cast(neg,dtype=tf.int32)
-
         zero=tf.constant(0, dtype=tf.float32)
         ## Gather postives
         indpos=tf.where(tf.not_equal(yt, zero))
         indpos=tf.cast(tf.reshape(indpos,[-1]),dtype=tf.int32)
         yp_p=tf.gather(yp,indpos)
 
-        ## Gather hard negatives
-        indneg=tf.where(tf.equal(yt, zero))
-        indneg=tf.cast(tf.reshape(indneg,[-1]),dtype=tf.int32)
-        yp_n=tf.gather(yp,indneg)
-        yp_n,ind=tf.nn.top_k(yp_n,neg)
-
-        return tf.reduce_mean(yp_p)#/(tf.reduce_mean(yp_n)+0.001)
+        return tf.reduce_mean(yp_p)
 
 
 
