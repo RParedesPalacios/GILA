a977778642425cdfe36caa4f05386a7da155732f
Roberto Paredes <rparedes@dsic.upv.es>
2018-11-15 13:00:06 +0100

    src

--- src/detect_loss.py
+++ src/detect_loss.py
@@ -9,42 +9,40 @@ def hnm_loss(y_true,y_pred):
 
     ## Count positives and define neg as pos
     pos=tf.cast(tf.count_nonzero(yt),dtype=tf.int32)
-    if (pos==0):
-        return 0.0
-    else:
-        zero=tf.constant(0, dtype=tf.float32)
 
-        ## Gather postives
-        indpos=tf.where(tf.not_equal(yt, zero))
-        indpos=tf.cast(tf.reshape(indpos,[-1]),dtype=tf.int32)
-        yp_p=tf.gather(yp,indpos)
+    zero=tf.constant(0, dtype=tf.float32)
 
-        ## Gather hard negatives
-        indneg=tf.where(tf.equal(yt, zero))
-        indneg=tf.cast(tf.reshape(indneg,[-1]),dtype=tf.int32)
-        yp_n=tf.gather(yp,indneg)
-        neg=3*pos
-        neg=tf.cast(neg,dtype=tf.int32)
+    ## Gather postives
+    indpos=tf.where(tf.not_equal(yt, zero))
+    indpos=tf.cast(tf.reshape(indpos,[-1]),dtype=tf.int32)
+    yp_p=tf.gather(yp,indpos)
 
-        yp_n,ind=tf.nn.top_k(yp_n,neg,sorted=True)
-        mask=tf.greater(yp_n,0.5)
-        yp_n=tf.boolean_mask(yp_n,mask)
+    ## Gather hard negatives
+    indneg=tf.where(tf.equal(yt, zero))
+    indneg=tf.cast(tf.reshape(indneg,[-1]),dtype=tf.int32)
+    yp_n=tf.gather(yp,indneg)
+    neg=tf.maximum(1,3*pos)
+    neg=tf.cast(neg,dtype=tf.int32)
 
-        lenp=tf.size(yp_p)
-        lenn=tf.size(yp_n)
+    yp_n,ind=tf.nn.top_k(yp_n,neg,sorted=True)
+    mask=tf.greater(yp_n,0.5)
+    yp_n=tf.boolean_mask(yp_n,mask)
 
-        ## Concat predicted both
-        yp_p=tf.Print(yp_p,[yp_p],"Pos:")
-        yp_n=tf.Print(yp_n,[yp_n],"Neg:")
-        myp=tf.concat([yp_p,yp_n],0)
+    lenp=tf.size(yp_p)
+    lenn=tf.size(yp_n)
 
-        ## Define targets (pos 1s and neg 0s)
-        yt1=tf.ones([lenp], tf.float32)
-        yt2=tf.zeros([lenn], tf.float32)
-        myt=tf.concat([yt1, yt2], 0)
+    ## Concat predicted both
+    yp_p=tf.Print(yp_p,[yp_p],"Pos:")
+    yp_n=tf.Print(yp_n,[yp_n],"Neg:")
+    myp=tf.concat([yp_p,yp_n],0)
 
-        return tf.reduce_sum(yt1 - yp_p)+tf.reduce_sum(yp_n)
+    ## Define targets (pos 1s and neg 0s)
+    yt1=tf.ones([lenp], tf.float32)
+    yt2=tf.zeros([lenn], tf.float32)
+    myt=tf.concat([yt1, yt2], 0)
 
+    #return tf.reduce_sum(yt1 - yp_p)+tf.reduce_sum(yp_n)
+    return tf.reduce_mean(tf.square(myt - myp))
 
 def num_pos(y_true, y_pred):
     ## reshape to 1D vectors
