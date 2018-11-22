d6f78dba066e62e01540e91e6cd771530713ab96
Roberto Paredes Palacios <rparedes@dsic.upv.es>
2018-11-22 22:37:15 +0100

    src

--- src/detect_loss.py
+++ src/detect_loss.py
@@ -11,7 +11,7 @@ def hnm_loss(y_true,y_pred):
     pos=tf.cast(tf.count_nonzero(yt),dtype=tf.int32)
     zero=tf.constant(0, dtype=tf.float32)
     pos=tf.Print(pos,[pos],"Pos=")
-    
+
     ## Gather postives
     indpos=tf.where(tf.not_equal(yt, zero))
     indpos=tf.cast(tf.reshape(indpos,[-1]),dtype=tf.int32)
@@ -41,7 +41,8 @@ def hnm_loss(y_true,y_pred):
     ln=tf.cast(lenn,dtype=tf.float32)
     lp=tf.cast(lenp,dtype=tf.float32)
 
-    return tf.reduce_mean(tf.square(myt - myp))
+    #return tf.reduce_mean(tf.square(myt - myp))
+    return tf.reduce_mean(yt1-yp_p)+tf.reduce_mean(yp_n)
 
 def num_pos(y_true, y_pred):
     ## reshape to 1D vectors
@@ -62,6 +63,7 @@ def num_pos(y_true, y_pred):
         indneg=tf.where(tf.equal(yt, zero))
         indneg=tf.cast(tf.reshape(indneg,[-1]),dtype=tf.int32)
         yp_n=tf.gather(yp,indneg)
+
         neg=tf.maximum(1,3*pos)
         neg=tf.cast(neg,dtype=tf.int32)
         yp_n,ind=tf.nn.top_k(yp_n,neg,sorted=True)
