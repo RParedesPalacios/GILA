45b6dbc1bb8850375f258e3ef272be4f526e5c3c
Roberto Paredes Palacios <rparedes@dsic.upv.es>
2018-11-22 17:40:52 +0100

    src

--- src/detect_pretrainedmodel.py
+++ src/detect_pretrainedmodel.py
@@ -14,6 +14,7 @@ def detect_pretrained_model(args,anchors,catlen):
 
     maps=[]
     for l in load_model.layers:
+        print(l.name)
         if (min(l.shape[1],l.shape[2])>=args.minmap)and(max(l.shape[1],l.shape[2])<=args.maxmap):
             print(l.name,l.shape[1],"x",l.shape[2])
             maps.append(l)
